8bd999d3eb7c4ccc8ee2c581ccf8467ea895d756
pjlplourde <pierre@spartancsharp.net>
2022-01-25 11:34:07 -0500

    Added CosmosDB Data Access Library
    Internal CosmosDBDataAccess handles direct communication with CosmosDB. Public CosmosDBCRUD is the API with the model types that bridge the JSON documents and the UI models.  Model classes created.  Appsettings.json file scaffolded, needs instance data from CosmosDB database.

---
+++ DataAccessLibrary/Models/ManagerModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary.Models
+{
+	public class ManagerModel : WorkerModel
+	{
+		public decimal Bonus { get; set; }
+	}
+}
---
+++ DataAccessLibrary/Models/WorkerModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+
+namespace DataAccessLibrary.Models
+{
+	public abstract class WorkerModel
+	{
+		[JsonProperty(PropertyName = "id")]
+		public Guid Id { get; set; } = Guid.NewGuid();
+		public string FirstName { get; set; } = string.Empty;
+		[JsonProperty(PropertyName = "partitionKey")]
+		public string LastName { get; set; } = string.Empty;
+		public int Age { get; set; }
+		public DateOnly Birthday { get; set; }
+		public decimal Wage { get; set; }
+		public int NumberOfHoursWorked { get; set; }
+		public int EmployeeId { get; set; }
+		public DateOnly HireDate { get; set; }
+		public DateOnly? TerminationDate { get; set; }
+		public string EmployeeType { get; set; } = string.Empty;
+
+		public override string? ToString()
+		{
+			return JsonConvert.SerializeObject(this);
+			;
+		}
+	}
+}
